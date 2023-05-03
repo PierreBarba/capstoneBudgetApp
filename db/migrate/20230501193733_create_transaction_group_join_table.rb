bd170a8e880f75722279d04538956f69ab80460c
Pierre Barba <99061965+PierreBarba@users.noreply.github.com>
2023-05-03 13:34:28 -0600

    Delete migration conflict

--- db/migrate/20230501193733_create_transaction_group_join_table.rb
+++
@@ -1,8 +0,0 @@
-class CreateTransactionGroupJoinTable < ActiveRecord::Migration[7.0]
-  def change
-    create_table :transactions_groups, id: false do |t|
-      t.belongs_to :transaction
-      t.belongs_to :group
-    end
-  end
-end
