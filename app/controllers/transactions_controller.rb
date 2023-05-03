22b81fc8e40fc694ed0874c45fb234b64169d873
Pierre Barba <99061965+PierreBarba@users.noreply.github.com>
2023-05-03 13:33:59 -0600

    Delete transaction controller

--- app/controllers/transactions_controller.rb
+++
@@ -1,8 +0,0 @@
-class TransactionsController < ApplicationController
-
-    def new
-        @current_user = current_user
-        @transaction = Transaction.new
-        @groups = current_user.groups
-      end
-end
